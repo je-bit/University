5235d979adfebc9cbffd914bbfaf1ae21434c604
je-bit <djenni_2001@abv.bg>
2020-06-03 21:49:57 +0300

    my project

---
+++ Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Test.Controllers
+{
+    public class TestController : Controller
+    {
+        public TestController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
